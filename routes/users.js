1e9ac07c3dcb0d84b80e1bd0e1353af5bbb4727b
Robin <hahne.robin@gmail.com>
2019-03-04 12:18:44 +0100

    register email sent works

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,8 @@ var express = require('express');
 var router = express.Router();
 const bcrypt = require('bcrypt');
 const User = require('../models/user.js')
+const nodemailer = require('nodemailer')
+const templates = require('../templates/template')
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
@@ -17,9 +19,38 @@ router.get('/login', function(req, res, next) {
 });
 
 router.post('/register', (req, res) => {
-  var eMailToRegister = req.body.eMail;
-  
+  //var eMailToRegister = req.body.eMail;
+  debugger
+  var mail = require('../nodeMailerWithTemp');
+  debugger
+  mail.sendPasswordReset('hahne.robin@gmail.com', 'Bobbie','Bob','http://localhost:3000/users')
 })
+
+router.post('/send-email', (req, res, next) => {
+  debugger
+  let { email } = req.body;
+  let transporter = nodemailer.createTransport({
+    service: 'Gmail',
+    auth: {
+      user: 'ironimpressioner@gmail.com',
+      pass: 'alwayshalffull' 
+    }
+  });
+  transporter.sendMail({
+    from: '"Iron Impression 👻" <ironimpressioner@gmail.com>',
+    to: email, 
+    subject: 'Iron Impression Get Access!',
+    html: templates.templateExample()
+  })
+  .then(info => {
+    res.render('users/message', { email })
+    console.log(info)
+  })
+  .catch(error => console.log(error))
+});
+
+
+
 
 router.post('/signup', (req, res) => {
   bcrypt.hash(req.body.password, 10, function (err, hash) {
